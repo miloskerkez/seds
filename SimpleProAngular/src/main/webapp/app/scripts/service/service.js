f3901f862ec3550c4bf1d9e78b13227884480e9f
Milos Kerkez <milos.kerkez@devtechgroup.com>
2015-04-30 10:49:29 +0200

    finish

--- SimpleProAngular/src/main/webapp/app/scripts/service/service.js
+++ SimpleProAngular/src/main/webapp/app/scripts/service/service.js
@@ -6,50 +6,14 @@
 
     function Service($http, $q) {
 
-        var urlPrefix = "scripts/data/";
-        var urlSuffix = ".json";
         var baseUrl = "/api/";
         var service = {
-            getFromServer: getFromServer,
-            getDogs: getDogs,
             setDataCRUD: setDataCRUD,
             getAllData: getAllData,
             buyPlayerService: buyPlayerService
         };
         return service;
 
-        /**
-         * receive parameter for url and return data from that url
-         * @param url
-         * @returns {jQuery.promise|promise.promise|d.promise|promise|.ready.promise|jQuery.ready.promise|*}
-         */
-        function getFromServer(param) {
-            var deferred = $q.defer();
-            $http.get(urlPrefix + param + urlSuffix)
-                .success(function (data) {
-                    deferred.resolve(data);
-                })
-                .error(function () {
-                    deferred.reject("bad");
-                });
-            return deferred.promise;
-        }
-
-        /**
-         * return list of dogs from db
-         * @returns {*}
-         */
-        function getDogs() {
-            var deferred = $q.defer();
-            $http.get(baseUrl + 'getDogs')
-                .success(function (data) {
-                    deferred.resolve(data);
-                })
-                .error(function () {
-                    deferred.reject("couldn't pull data from module!");
-                });
-            return deferred.promise;
-        }
 
         /**
          * send data to server and get response, work for CRUD operations with post methods
